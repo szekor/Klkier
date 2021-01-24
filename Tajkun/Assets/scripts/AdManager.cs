dcce37def19e2a26c1977e505340ff12823a34d0
Marconeti1 <bartoszlegocki@o2.pl>
2021-01-24 17:14:11 +0100

    bla bla
    kolejny push

--- Tajkun/Assets/scripts/AdManager.cs
+++ Tajkun/Assets/scripts/AdManager.cs
@@ -3,17 +3,65 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class AdManager : MonoBehaviour
+public class AdManager : MonoBehaviour   ,IUnityAdsListener
 {
-    // Start is called before the first frame update
-    void Start()
+#if UNITY_ANDROID
+    private static readonly string storeID = "3986221";
+#elif UNITY_IOS
+    private static readonly string storeID = "3986220";
+#endif
+    private static readonly string videoID = "video";
+    private static readonly string bannerID = "baner";
+
+#if UNITY_EDITOR
+    private static bool testMode = true;
+#else
+private static bool testMode = false;
+#endif
+    public static AdManager instance;
+    private void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            Advertisement.AddListener(this);
+            Advertisement.Initialize(storeID, testMode);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+            
+                
+                
+                }
+    public static void ShowStandardAd()
+    {
+        if (Advertisement.IsReady(videoID))
+            Advertisement.Show(videoID);
+    }
+    private static IEnumerator ShowBannerWhenReady()
+    {
+        while (!Advertisement.IsReady(bannerID))
+            yield return new WaitForSeconds(0.5f);
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(bannerID);
+    }
+    public static void ShowBanner()
+    {
+        instance.StartCoroutine(ShowBannerWhenReady());
+    }
+    public static void HideBanner()
+    {
+        Advertisement.Banner.Hide();
+    }
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public void OnUnityAdsDidError(string message) { }
+    public void OnUnityAdsDidStart(string placementId) { }
+    public void OnUnityAdsReady(string placementId) { }
 }
