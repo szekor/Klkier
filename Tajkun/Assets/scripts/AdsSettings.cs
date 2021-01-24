75a3d1ea0cc44ae58d1abc111c83d86d1bfa87f2
Marconeti1 <bartoszlegocki@o2.pl>
2021-01-24 18:08:31 +0100

    dzialajacy banr
    baner ready

---
+++ Tajkun/Assets/scripts/AdsSettings.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public static class Settings
+    {
+        public static string GameId = "3986221";
+        public static bool TestMode = true;
+    }
+
--- Tajkun/Assets/scripts/banerAD.cs
+++ Tajkun/Assets/scripts/banerAD.cs
@@ -1,18 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
+using System;
 
 public class banerAD : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string banerad = "bannerad";
     void Start()
     {
-        
+        Advertisement.Initialize(Settings.GameId, Settings.TestMode);
+        StartCoroutine(ShowBanner());
+
+    }
+    IEnumerator ShowBanner()
+    {
+        while(!Advertisement.IsReady(banerad))
+        {
+            yield return new WaitForSeconds(0.5f);
+        }
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+        Advertisement.Banner.Show(banerad);
+        Debug.Log("pokazano baner");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
